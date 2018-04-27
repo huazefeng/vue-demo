e711e203f89dc4df2f9875afc2e96f563b1c58d0
huazefeng <1064945472@qq.com>
2018-04-27 17:34:49 +0800

    '熟悉vuex的使用'

--- vue-music/src/api/singer.js
+++ vue-music/src/api/singer.js
@@ -19,3 +19,22 @@ export function getSingerList() {
 
     return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+    const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+    
+    const data = Object.assign({}, commonParams, {
+        loginUin: 0,
+        hostUin: 0,
+        notice: 0,
+        platform: 'yqq',
+        needNewCode: 0,
+        order: 'listen',
+        num: 100,
+        begin: 0,
+        songstatus: 1,
+        singermid: singerId
+    })
+
+    return jsonp(url, data, options)
+}
--- vue-music/src/common/js/song.js
+++ vue-music/src/common/js/song.js
@@ -1,4 +1,4 @@
-import {getLyric} from 'api/song'
+// import {getLyric} from 'api/song'
 import {ERR_OK} from 'api/config'
 import {Base64} from 'js-base64'
 
@@ -32,6 +32,7 @@ export default class Song {
   }
 }
 
+// 工厂方法创建Song对象
 export function createSong(musicData) {
   return new Song({
     id: musicData.songid,
@@ -41,7 +42,8 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: `http://isure.stream.qqmusic.qq.com/C100${musicData.songmid}.m4a?fromtag=32`
   })
 }
 
